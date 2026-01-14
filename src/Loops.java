eda01ecd24c9a6df949e145a855b089a9782e1dc
Mike <MikeGao2024@outlook.com>
2026-01-14 07:39:31 -0500

    Graded Loop hw

---
+++ src/Loops.java
@@ -0,0 +1,41 @@
+public class Loops {
+    public static void main(String[] args) {
+        Loops myCount = new Loops();
+    }
+
+    public Loops(){
+
+        nestedLoop();
+    }
+
+    public void countUP(){
+        for(int i=1; i < 6; i++){
+            System.out.println(i);
+        }
+    }
+
+    public void countByThrees(){
+        for(int i=3; i < 16; i = i+3){
+            System.out.print(i);
+        }
+    }
+
+    public void countDown(){
+        for(int i=10; i > 0; i = --i){
+            System.out.print(i + ", ");
+        }
+        System.out.println("Happy Loop Year!");
+    }
+
+    public void nestedLoop(){
+
+        for(int i=1; i<6; i++){
+            System.out.println();
+            for(int j=1; j<=i; j++){
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+
+}
